ac508bc9356a2b258cc4c1c181b02f338ef5b23f
31-5 <xxxzaharka@gmail.com>
2023-12-15 13:20:28 +0500

    Initial commit

---
+++ app/src/main/java/com/example/fritaty/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.fritaty
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+// Splash screen https://www.youtube.com/watch?v=WZWr0Abomfw
